608bb4e27d018961db0c1914bbb4b4007b2cb999
young <beyoung@amazon.com>
2019-05-17 12:47:39 +0000

    use secrets manager for python

--- createWebHook/lambda_function.py
+++ createWebHook/lambda_function.py
@@ -1,28 +1,32 @@
-import json
-import boto3
+import json, boto3, os, json
 from botocore.vendored import requests
 """
 event: {
     projectName: codebuild project name
     hooks_url: git repos hooks url
     usr: git ee login user
-    token: git ee personal access token
 }
 """
 def lambda_handler(event, context):
     codebuild = boto3.client('codebuild')
     project = event['projectName']
-    
+
     # call api to create webhook
     response = codebuild.create_webhook(projectName=project)
     if not 'webhook' in response:
         return response.json()
     
     webhook = response['webhook']
+    
+    # Get token
+    sm = boto3.client('secretsmanager')
+    secretName = os.environ['SECRET_NAME']
+    result = sm.get_secret_value(SecretId=secretName)
+    secretsStore = json.loads(result['SecretString'])
+    token = secretsStore['GITEE_ACCESS_TOKEN']
 
     hooks_url = event['hooks_url']
     usr = event['usr']
-    token = event['token']
     payload = '''{
         "name": "web",
         "active": true,
